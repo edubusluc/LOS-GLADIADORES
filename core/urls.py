e5569909e42b93ec967c1e3499e3704064723f3c
edubusluc <edubusluc@alum.us.es>
2024-10-22 16:48:27 +0200

    fix: home urls

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,6 @@ from players.views import *
 from django.contrib.auth import views as auth_views
 
 urlpatterns = (
-    path("", home, name="home"),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 )
--- post/urls.py
+++ post/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import home, create_post
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', home, name='home'), 
     path('create/', create_post, name='create_post'),
 ]

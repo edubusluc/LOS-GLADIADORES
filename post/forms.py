14537d3ef643b96b4eafe6db98d08150dfe87f0a
edubusluc <edubusluc@alum.us.es>
2024-10-25 13:10:47 +0200

    add: Galery Post

--- post/forms.py
+++ post/forms.py
@@ -4,4 +4,4 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'image']
+        fields = ['title','content']
--- post/views.py
+++ post/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .models import Post
+from .models import Post, Image
 from .forms import PostForm
 from match.models import Match
 
 def home(request):
-    posts = Post.objects.all().order_by('-created_at')
+    posts = Post.objects.prefetch_related('images').all().order_by('-created_at')
     return render(request, 'home.html', {'posts': posts})
 
 
@@ -13,20 +13,31 @@ def home(request):
 def create_post(request):
     matches = Match.objects.filter(draft_mode=False)
     if request.method == 'POST':
-        form = PostForm(request.POST, request.FILES)
-        match_id = request.POST.get('match_id')  # Obtener el ID del partido seleccionado
-        if form.is_valid():
-            post = form.save(commit=False)
+        post_form = PostForm(request.POST)
+        files = request.FILES.getlist('images')  # Obtener los archivos subidos
 
-            if match_id:  # Si se ha seleccionado un partido
+        if post_form.is_valid() and files:
+            post = post_form.save(commit=False)
+
+            match_id = request.POST.get('match_id')  # Obtener el ID del partido seleccionado
+            if match_id:
                 try:
                     match = Match.objects.get(id=match_id)
-                    post.content = f"{match.result}, en el partido {match.local} vs {match.visiting} con resultado final: {match.result_points}"  # O la lógica que desees para crear el contenido
+                    post.content = f"{match.result}, en el partido {match.local} vs {match.visiting} con resultado final: {match.result_points}"
                 except Match.DoesNotExist:
-                    post.content = "Partido no válido."  # Manejo de errores si es necesario
-            
+                    post.content = "Partido no válido."
+
             post.save()
+
+            # Guardar cada imagen asociada a la publicación
+            for file in files:
+                Image.objects.create(post=post, image=file)
+
             return redirect('home')
     else:
-        form = PostForm()
-    return render(request, 'create_post.html', {'form': form, "matches": matches})
+        post_form = PostForm()
+
+    return render(request, 'create_post.html', {
+        'post_form': post_form,
+        'matches': matches
+    })
